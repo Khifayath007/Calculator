46ac5f3a048ef0fced26d8d6472d38350e30b8a0
Khifayath007 <ahmedkhifayath37@gmail.com>
2025-02-26 21:39:47 +0530

    updated

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-let buttons = document.querySelectorAll(".button")
-const calculate  = (btnValue) => {
-    console.log(btnValue);
-} 
-buttons.forEach(button => {
-    button.addEventListener("click", (e) => calculate(e.target.dataset.value));
-});
+let string = "";
+let num = 0;
+let cl = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click' , (e) =>{
+        if(e.target.innerHTML == "="){
+            try{
+                string = eval(string);
+                document.querySelector(".display").innerHTML = string
+            }
+            catch(error){
+                string = "error"
+                document.querySelector(".display").innerHTML = string
+            }
+        }
+        else if(e.target.innerHTML === "AC"){
+            document.querySelector('.display').innerHTML = "";
+            string = "";
+        }
+        else if(e.target.innerHTML === "DEL"){
+            document.querySelector('.display').innerHTML = string.substring(0,string.length-1);
+            string = string.substring(0,string.length-1)
+        }
+        else{
+            string = string + e.target.innerHTML;
+            document.querySelector(".display").innerHTML = string
+        }
+    })
+})
